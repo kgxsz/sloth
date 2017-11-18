b35fd5f20a18bc7b8777bf0497a3d7162f187493
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-11-17 21:32:42 -0300

    Add normalize and some banal styling

--- src/main/styles/core.clj
+++ src/main/styles/core.clj
@@ -1,9 +1,15 @@
 (ns styles.core
   (:require [garden.def :refer [defstylesheet defstyles]]
-            [garden.units :refer [px]]))
+            [garden.units :refer [px]]
+            [normalize.core :refer [normalize]]))
 
 (defstyles app
+  normalize
   [:body
    {:font-family "sans-serif"
     :font-size (px 16)
-    :line-height 1.5}])
+    :line-height 1.5}]
+  [:app
+   {:width (px 100)
+    :height (px 100)
+    :background-color :red}])
