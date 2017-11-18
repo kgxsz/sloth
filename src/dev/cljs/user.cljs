8e68eff2eeb6df9d28b01137edae6ffc50ac98f4
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-11-18 17:13:02 -0300

    Add a convenience client db dump function

--- src/dev/cljs/user.cljs
+++ src/dev/cljs/user.cljs
@@ -3,6 +3,20 @@
             [app.core :as core]
             [fulcro.client.core :as fc]))
 
-(defn refresh [] (swap! core/app fc/mount ui/App "app"))
+(defn dump
+  "Convenience function for printing the client db via the cljs repl."
+  [& keys]
+  (let [state-map @(om.next/app-state (-> core/app deref :reconciler))
+        data-of-interest (if (seq keys)
+                           (get-in state-map keys)
+                           state-map)]
+    data-of-interest))
+
+(defn refresh
+  "Convenience function for Figwheel to remount the app when files are edited."
+  []
+  (swap! core/app fc/mount ui/App "app"))
 
 (refresh)
+
+
