f8baf123cfc0fc30ca0d9a305a98af225ea8e238
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-12-04 18:34:45 -0300

    First pass at project configuration for uberjars

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject sloth "0.0.1"
-  :description "sloth"
+(defproject sloth "0.1.0-SNAPSHOT"
+
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.908"]
                  [org.omcljs/om "1.0.0-beta1"]
@@ -10,54 +10,78 @@
                  [clj-time "0.14.2"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]]
 
-  :source-paths ["src/main"]
   :resource-paths ["resources"]
+
   :clean-targets ^{:protect false} ["resources/public/js"
                                     "resources/public/css"
                                     "target"
-                                    ".nrepl-port"]
-
-  :min-lein-version "2.0.0"
+                                    ".nrepl-port"
+                                    ".lein-repl-history"]
 
   :plugins [[lein-cljsbuild "1.1.6"]
             [lein-garden "0.3.0"]]
 
-  :aliases {"build" ["do"
-                     "clean"
-                     ["cljsbuild" "once" "min"]]}
-
-  :prep-tasks [["garden" "once"]]
-
-  :cljsbuild {:builds
-              [{:id           "dev"
-                :source-paths ["src/main" "src/dev"]
-
-                :figwheel     {:on-jsload "cljs.user/refresh"}
-                :compiler     {:main                 cljs.user
-                               :output-to            "resources/public/js/app.js"
-                               :output-dir           "resources/public/js/app"
-                               :preloads             [devtools.preload]
-                               :asset-path           "js/app"
-                               :optimizations        :none}}
-               {:id           "min"
-                :source-paths ["src/main"]
-                :compiler     {:main            app.main
-                               :output-to       "resources/public/js/app.js"
-                               :optimizations   :advanced
-                               :pretty-print    false}}]}
-
-  :figwheel {:css-dirs ["resources/public/css"]
-             :server-logfile "target/figwheel_temp/logs/figwheel_server.log"}
-
-  :garden {:builds [{:source-paths ["src/main"]
-                     :stylesheet styles.core/app
-                     :compiler {:output-to "resources/public/css/app.css"
-                                :pretty-print? false}}]}
-
   :profiles {:dev {:source-paths ["src/dev" "src/main"]
+                   :min-lein-version "2.0.0"
+
+                   :plugins [[lein-cljsbuild "1.1.6"]
+                             [lein-garden "0.3.0"]]
+
+                   :omit-source true
+
+                   :aliases {"build" ["do"
+                                      "clean"
+                                      ["cljsbuild" "once" "min"]]}
+
+                   :prep-tasks [["garden" "once"]]
+
+                   :cljsbuild {:builds
+                               [{:id "dev"
+                                 :source-paths ["src/main" "src/dev"]
+
+                                 :figwheel {:on-jsload "cljs.user/refresh"}
+                                 :compiler {:main cljs.user
+                                            :output-to "resources/public/js/app.js"
+                                            :output-dir "resources/public/js/app"
+                                            :preloads [devtools.preload]
+                                            :asset-path "js/app"
+                                            :optimizations :none}}
+                                {:id           "min"
+                                 :source-paths ["src/main"]
+                                 :compiler     {:main app.main
+                                                :output-to "resources/public/js/app.js"
+                                                :optimizations :advanced
+                                                :pretty-print false}}]}
+
+                   :figwheel {:css-dirs ["resources/public/css"]
+                              :server-logfile "target/figwheel_temp/logs/figwheel_server.log"}
+
+                   :garden {:builds [{:source-paths ["src/main"]
+                                      :stylesheet styles.core/app
+                                      :compiler {:output-to "resources/public/css/app.css"
+                                                 :pretty-print? false}}]}
+
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :dependencies [[binaryage/devtools "0.9.4"]
                                   [com.cemerick/piggieback "0.2.1"]
                                   [org.clojure/tools.namespace "0.3.0-alpha4"]
                                   [figwheel-sidecar "0.5.13"]
-                                  [org.clojure/tools.nrepl "0.2.13"]]}})
+                                  [org.clojure/tools.nrepl "0.2.13"]]}
+
+             :uberjar {:source-paths ["src/main"]
+                       :main app.main
+                       :aot [app.main]
+                       :uberjar-name "sloth-standalone.jar"
+                       :prep-tasks ["compile"
+                                    ["cljsbuild" "once" "min"]
+                                    ["garden" "once"]]
+                       :garden {:builds [{:source-paths ["src/main"]
+                                          :stylesheet styles.core/app
+                                          :compiler {:output-to "resources/public/css/app.css"
+                                                     :pretty-print? false}}]}
+                       :cljsbuild  {:builds [{:id "min"
+                                              :source-paths ["src/main"]
+                                              :compiler {:main app.main
+                                                         :output-to "resources/public/js/app.js"
+                                                         :optimizations :advanced
+                                                         :pretty-print false}}]}}})
