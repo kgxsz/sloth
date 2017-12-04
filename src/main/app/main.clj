6315cdb0c4038e70f52ea585997c11e4366edf29
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-12-04 18:33:32 -0300

    Main entry point

--- src/main/app/main.clj
+++ src/main/app/main.clj
@@ -1,3 +1,9 @@
-(ns app.main)
+(ns app.main
+  (:gen-class)
+  (:require [app.operations :as operations]
+            [app.server :as server]
+            [com.stuartsierra.component :as component]))
 
-(println "hello")
+(defn -main [& args]
+  (let [system (server/make-system "config/prod.edn")]
+    (component/start system)))
