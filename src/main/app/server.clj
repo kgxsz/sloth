3558150fcf842073e14237dc8b67108ef0d219c8
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-12-04 18:33:51 -0300

    Clean up

--- src/main/app/server.clj
+++ src/main/app/server.clj
@@ -1,9 +1,8 @@
 (ns app.server
-  (:require [fulcro.easy-server :as easy]
-            [fulcro.server :as server]
-            [taoensso.timbre :as timbre]))
+  (:require [fulcro.easy-server :as easy-server]
+            [fulcro.server :as server]))
 
 (defn make-system [config-path]
-  (easy/make-fulcro-server
+  (easy-server/make-fulcro-server
    :config-path config-path
    :parser (server/fulcro-parser)))
