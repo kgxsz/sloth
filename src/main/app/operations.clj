472a5eccc0975db156bceb0573ffa8f0f01a81bd
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-01-27 15:11:53 +0000

    Get some dumb datomic pulling in place

--- src/main/app/operations.clj
+++ src/main/app/operations.clj
@@ -7,7 +7,6 @@
 
 ;; TODO - somehow shoehorn this db stuff somewhere
 (comment
-
   ;; make a db-uri when running transactor
   (def db-uri "datomic:dev://localhost:4334/core")
 
@@ -17,8 +16,98 @@
   ;; create the database
   (d/create-database db-uri)
 
+  ;; or delete the database
+  (d/delete-database db-uri)
+
   ;; create the connection to the db
   (def conn (d/connect db-uri))
+
+  ;; create the schemas
+  (def user-schema [{:db/doc "A user's first name"
+                     :db/ident :user/first-name
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/id #db/id [:db.part/db]
+                     :db.install/_attribute :db.part/db}
+                    {:db/doc "A user's last name"
+                     :db/ident :user/last-name
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/id #db/id [:db.part/db]
+                     :db.install/_attribute :db.part/db}
+                    {:db/doc "A user's avatar url"
+                     :db/ident :user/avatar-url
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/id #db/id [:db.part/db]
+                     :db.install/_attribute :db.part/db}
+                    {:db/doc "A user's calendars"
+                     :db/ident :user/calendars
+                     :db/valueType :db.type/ref
+                     :db/cardinality :db.cardinality/many
+                     :db/id #db/id [:db.part/db]
+                     :db.install/_attribute :db.part/db}])
+
+  (def calendar-schema [{:db/doc "A calendar's title"
+                         :db/ident :calendar/title
+                         :db/valueType :db.type/string
+                         :db/cardinality :db.cardinality/one
+                         :db/id #db/id [:db.part/db]
+                         :db.install/_attribute :db.part/db}
+                        {:db/doc "A calendar's subtitle"
+                         :db/ident :calendar/subtitle
+                         :db/valueType :db.type/string
+                         :db/cardinality :db.cardinality/one
+                         :db/id #db/id [:db.part/db]
+                         :db.install/_attribute :db.part/db}])
+
+  ;; add the schemas
+  @(d/transact conn user-schema)
+
+  @(d/transact conn calendar-schema)
+
+  ;; create a calendars transaction
+  (def temp-calendar-id (d/tempid :db.part/user))
+
+  (def calendars-tx [{:db/id temp-calendar-id
+                      :calendar/title "Some Title"
+                      :calendar/subtitle "some subtitle"}])
+
+  ;; apply the calendars transaction
+  (def tx-result @(d/transact conn calendars-tx))
+
+  ;; resolve the temp id
+  (def calendar-id (d/resolve-tempid (:db-after tx-result) (:tempids tx-result) temp-calendar-id))
+
+  ;; check the entity
+  (d/touch (d/entity (d/db conn) calendar-id))
+
+  ;; create a users transaction
+  (def temp-user-id (d/tempid :db.part/user))
+
+  (def users-tx [{:db/id temp-user-id
+                  :user/first-name "Jacob"
+                  :user/last-name "Suzukawa"
+                  :user/avatar-url "images/avatar.jpg"
+                  :user/calendars #{17592186045419}}])
+
+  ;; apply the users transaction
+  (def tx-result @(d/transact conn users-tx))
+
+  ;; resolve the temp id
+  (def user-id (d/resolve-tempid (:db-after tx-result) (:tempids tx-result) temp-user-id))
+
+  ;; check the entity
+  (d/touch (d/entity (d/db conn) user-id))
+
+  ;; pull the user and the calendars
+  (d/pull (d/db conn)
+          [:user/first-name
+           :user/last-name
+           :user/avatar-url
+           {:user/calendars [:calendar/title]}]
+          user-id)
+
 
   )
 
@@ -57,12 +146,14 @@
          (let [db @db]
            (om.next/db->tree query (get (:user/by-id db) current-user-id) db))))
 
-(defmutation add-checked-date!
+;; TODO - get some real mutations going on
+#_(defmutation add-checked-date!
   [{:keys [id date]}]
   (action [_]
           (swap! db update-in [:calendar/by-id id :calendar/checked-dates] conj date)))
 
-(defmutation remove-checked-date!
+;; TODO - get some real mutations going on
+#_(defmutation remove-checked-date!
   [{:keys [id date]}]
   (action [_]
           (swap! db update-in [:calendar/by-id id :calendar/checked-dates] disj date)))
