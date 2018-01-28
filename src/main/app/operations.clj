947c79b57ca2e02737a30077360874d2c15fad81
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-01-28 14:23:32 +0000

    Function for creating the dummy database

--- src/main/app/operations.clj
+++ src/main/app/operations.clj
@@ -5,112 +5,102 @@
             [fulcro.client.impl.application :as app]
             [om.next :as om]))
 
-;; TODO - somehow shoehorn this db stuff somewhere
+(defn create-dummy-db
+  []
+  (let [db-uri "datomic:mem://core"
+        delete-db-result (d/delete-database db-uri)
+        create-db-result (d/create-database db-uri)
+        conn (d/connect db-uri)
+        schema [{:db/doc "A user's first name"
+                 :db/ident :user/first-name
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A user's last name"
+                 :db/ident :user/last-name
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A user's avatar url"
+                 :db/ident :user/avatar-url
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A user's calendars"
+                 :db/ident :user/calendars
+                 :db/valueType :db.type/ref
+                 :db/cardinality :db.cardinality/many
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A calendar's title"
+                 :db/ident :calendar/title
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A calendar's subtitle"
+                 :db/ident :calendar/subtitle
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A calendar's colour option"
+                 :db/ident :calendar/colour-option
+                 :db/valueType :db.type/keyword
+                 :db/cardinality :db.cardinality/one
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}
+                {:db/doc "A calendar's checked dates"
+                 :db/ident :calendar/checked-dates
+                 :db/valueType :db.type/string
+                 :db/cardinality :db.cardinality/many
+                 :db/id #db/id [:db.part/db]
+                 :db.install/_attribute :db.part/db}]
+        schema-tx-result @(d/transact (d/connect db-uri) schema)
+        temp-calendar-id (d/tempid :db.part/user)
+        temp-user-id (d/tempid :db.part/user)
+        entities [{:db/id temp-calendar-id
+                   :calendar/title "Some Title"
+                   :calendar/subtitle "some subtitle"
+                   :calendar/colour-option :a
+                   :calendar/checked-dates #{"20171125" "20171126" "20171127" "20171128"}}
+                  {:db/id temp-user-id
+                   :user/first-name "Keigo"
+                   :user/last-name "Suzukawa"
+                   :user/avatar-url "images/avatar.jpg"
+                   :user/calendars [temp-calendar-id]}]
+        {:keys [db-after tempids]} @(d/transact conn entities)]
+
+    {:conn conn
+     :calendar-id (d/resolve-tempid db-after tempids temp-calendar-id)
+     :user-id (d/resolve-tempid db-after tempids temp-user-id)}))
+
 (comment
-  ;; make a db-uri when running transactor
-  (def db-uri "datomic:dev://localhost:4334/core")
 
-  ;; make a db-uri when not running a transactor
-  (def db-uri "datomic:mem://core")
+  (def dummy-db (create-dummy-db))
 
-  ;; create the database
-  (d/create-database db-uri)
+  (d/touch (d/entity (d/db (:conn dummy-db)) (:calendar-id dummy-db)))
 
-  ;; or delete the database
-  (d/delete-database db-uri)
+  (d/q '[:find ?e
+         :where [?e :user/first-name "Jacob"]]
+       (d/db (:conn dummy-db)))
 
-  ;; create the connection to the db
-  (def conn (d/connect db-uri))
-
-  ;; create the schemas
-  (def user-schema [{:db/doc "A user's first name"
-                     :db/ident :user/first-name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one
-                     :db/id #db/id [:db.part/db]
-                     :db.install/_attribute :db.part/db}
-                    {:db/doc "A user's last name"
-                     :db/ident :user/last-name
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one
-                     :db/id #db/id [:db.part/db]
-                     :db.install/_attribute :db.part/db}
-                    {:db/doc "A user's avatar url"
-                     :db/ident :user/avatar-url
-                     :db/valueType :db.type/string
-                     :db/cardinality :db.cardinality/one
-                     :db/id #db/id [:db.part/db]
-                     :db.install/_attribute :db.part/db}
-                    {:db/doc "A user's calendars"
-                     :db/ident :user/calendars
-                     :db/valueType :db.type/ref
-                     :db/cardinality :db.cardinality/many
-                     :db/id #db/id [:db.part/db]
-                     :db.install/_attribute :db.part/db}])
-
-  (def calendar-schema [{:db/doc "A calendar's title"
-                         :db/ident :calendar/title
-                         :db/valueType :db.type/string
-                         :db/cardinality :db.cardinality/one
-                         :db/id #db/id [:db.part/db]
-                         :db.install/_attribute :db.part/db}
-                        {:db/doc "A calendar's subtitle"
-                         :db/ident :calendar/subtitle
-                         :db/valueType :db.type/string
-                         :db/cardinality :db.cardinality/one
-                         :db/id #db/id [:db.part/db]
-                         :db.install/_attribute :db.part/db}])
-
-  ;; add the schemas
-  @(d/transact conn user-schema)
-
-  @(d/transact conn calendar-schema)
-
-  ;; create a calendars transaction
-  (def temp-calendar-id (d/tempid :db.part/user))
-
-  (def calendars-tx [{:db/id temp-calendar-id
-                      :calendar/title "Some Title"
-                      :calendar/subtitle "some subtitle"}])
-
-  ;; apply the calendars transaction
-  (def tx-result @(d/transact conn calendars-tx))
-
-  ;; resolve the temp id
-  (def calendar-id (d/resolve-tempid (:db-after tx-result) (:tempids tx-result) temp-calendar-id))
-
-  ;; check the entity
-  (d/touch (d/entity (d/db conn) calendar-id))
-
-  ;; create a users transaction
-  (def temp-user-id (d/tempid :db.part/user))
-
-  (def users-tx [{:db/id temp-user-id
-                  :user/first-name "Jacob"
-                  :user/last-name "Suzukawa"
-                  :user/avatar-url "images/avatar.jpg"
-                  :user/calendars #{17592186045419}}])
-
-  ;; apply the users transaction
-  (def tx-result @(d/transact conn users-tx))
-
-  ;; resolve the temp id
-  (def user-id (d/resolve-tempid (:db-after tx-result) (:tempids tx-result) temp-user-id))
-
-  ;; check the entity
-  (d/touch (d/entity (d/db conn) user-id))
-
-  ;; pull the user and the calendars
-  (d/pull (d/db conn)
-          [:user/first-name
+  (d/pull (d/db (:conn dummy-db))
+          [:db/id
+           :user/first-name
            :user/last-name
            :user/avatar-url
-           {:user/calendars [:calendar/title]}]
-          user-id)
-
+           {:user/calendars [:db/id
+                             :calendar/title
+                             :calendar/subtitle
+                             :calendar/colour-option
+                             :calendar/checked-dates]}]
+          (:user-id dummy-db))
 
   )
-
 
 ;; TODO - use sessions to avoid this
 (def current-user-id #uuid "d1cce9c9-171c-4616-ad61-d2990150dae2")
