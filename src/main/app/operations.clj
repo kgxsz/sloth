43493c8225cc831654754f5af3f30e2972c9b3f7
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-01-28 18:25:58 +0000

    Naive way to kick off a db

--- src/main/app/operations.clj
+++ src/main/app/operations.clj
@@ -2,16 +2,13 @@
   (:require [clj-time.core :as t]
             [datomic.api :as d]
             [fulcro.server :as server :refer [defquery-root defquery-entity defmutation]]
-            [fulcro.client.impl.application :as app]
-            [om.next :as om]))
+            [fulcro.client.impl.application :as app]))
+
+(def db-uri "datomic:mem://core")
 
 (defn create-dummy-db
   []
-  (let [db-uri "datomic:mem://core"
-        delete-db-result (d/delete-database db-uri)
-        create-db-result (d/create-database db-uri)
-        conn (d/connect db-uri)
-        schema [{:db/doc "A user's first name"
+  (let [schema [{:db/doc "A user's first name"
                  :db/ident :user/first-name
                  :db/valueType :db.type/string
                  :db/cardinality :db.cardinality/one
@@ -59,36 +56,39 @@
                  :db/cardinality :db.cardinality/many
                  :db/id #db/id [:db.part/db]
                  :db.install/_attribute :db.part/db}]
-        schema-tx-result @(d/transact (d/connect db-uri) schema)
         temp-calendar-id (d/tempid :db.part/user)
         temp-user-id (d/tempid :db.part/user)
         entities [{:db/id temp-calendar-id
                    :calendar/title "Some Title"
                    :calendar/subtitle "some subtitle"
                    :calendar/colour-option :a
-                   :calendar/checked-dates #{"20171125" "20171126" "20171127" "20171128"}}
+                   :calendar/checked-dates #{"20180110" "20180114" "20180122"}}
                   {:db/id temp-user-id
                    :user/first-name "Keigo"
                    :user/last-name "Suzukawa"
                    :user/avatar-url "images/avatar.jpg"
-                   :user/calendars [temp-calendar-id]}]
-        {:keys [db-after tempids]} @(d/transact conn entities)]
+                   :user/calendars #{temp-calendar-id}}]]
 
-    {:conn conn
-     :calendar-id (d/resolve-tempid db-after tempids temp-calendar-id)
-     :user-id (d/resolve-tempid db-after tempids temp-user-id)}))
+    (d/delete-database db-uri)
+    (d/create-database db-uri)
+    @(d/transact (d/connect db-uri) schema)
+    @(d/transact (d/connect db-uri) entities)))
 
 (comment
 
-  (def dummy-db (create-dummy-db))
-
-  (d/touch (d/entity (d/db (:conn dummy-db)) (:calendar-id dummy-db)))
+  (create-dummy-db)
 
   (d/q '[:find ?e
-         :where [?e :user/first-name "Jacob"]]
-       (d/db (:conn dummy-db)))
+         :where [?e :user/first-name "Keigo"]]
+       (d/db (d/connect db-uri)))
 
-  (d/pull (d/db (:conn dummy-db))
+  (d/q '[:find ?e
+         :where [?e :calendar/title "Some Title"]]
+       (d/db (d/connect db-uri)))
+
+  (d/touch (d/entity (d/db (d/connect db-uri)) 17592186045419))
+
+  (d/pull (d/db (d/connect db-uri))
           [:db/id
            :user/first-name
            :user/last-name
@@ -98,43 +98,36 @@
                              :calendar/subtitle
                              :calendar/colour-option
                              :calendar/checked-dates]}]
-          (:user-id dummy-db))
+          17592186045419)
 
-  )
+  ;; TODO - make this part of the system startup
+  (create-dummy-db)
 
-;; TODO - use sessions to avoid this
-(def current-user-id #uuid "d1cce9c9-171c-4616-ad61-d2990150dae2")
+  #_(om.next/db->tree [:db/id
+                       :user/first-name
+                       :user/last-name
+                       :user/avatar-url
+                       {:user/calendars [:db/id
+                                         :calendar/title
+                                         :calendar/subtitle
+                                         :calendar/colour-option
+                                         :calendar/checked-dates]}]
+                      (get (:user/by-id @db) current-user-id)
+                      @db)
 
-;; TODO - use a real db
-(def db (atom {:user/by-id {#uuid "d1cce9c9-171c-4616-ad61-d2990150dae2"
-                            {:db/id 17592186045418
-                             :user/first-name "Keigo"
-                             :user/last-name "Suzukawa"
-                             :user/avatar-url "images/avatar.jpg"
-                             :user/calendars [[:calendar/by-id 17592186045419]]}}
-               :calendar/by-id {17592186045419
-                                {:db/id 17592186045419
-                                 :calendar/title "Cardio Training"
-                                 :calendar/subtitle "freeletics or running"
-                                 :calendar/colour-option :a
-                                 :calendar/checked-dates #{"20171125" "20171126" "20171127" "20171128"}}}}))
+  (defn get-current-user-id []
+    (->> (d/db (d/connect db-uri))
+         (d/q '[:find ?e
+                :where [?e
+                        :user/first-name "Keigo"]])
+         (ffirst)))
 
-(om.next/db->tree [:db/id
-                   :user/first-name
-                   :user/last-name
-                   :user/avatar-url
-                   {:user/calendars [:db/id
-                                     :calendar/title
-                                     :calendar/subtitle
-                                     :calendar/colour-option
-                                     :calendar/checked-dates]}]
-                  (get (:user/by-id @db) current-user-id)
-                  @db)
-
-(defquery-root :current-user
-  (value [{:keys [query]} params]
-         (let [db @db]
-           (om.next/db->tree query (get (:user/by-id db) current-user-id) db))))
+  (defquery-root :current-user
+    (value [{:keys [query]} params]
+           (let [db (d/db (d/connect db-uri))
+                 current-user-id (get-current-user-id)]
+             (d/pull db query current-user-id)
+             #_(om.next/db->tree query (get (:user/by-id db) current-user-id) db)))))
 
 ;; TODO - get some real mutations going on
 #_(defmutation add-checked-date!
