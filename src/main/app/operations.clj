8a1384c19c178ab86b4422b274e05dd88dd971e7
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-01-29 23:11:14 +0000

    DB gets created as a component

--- src/main/app/operations.clj
+++ src/main/app/operations.clj
@@ -1,91 +1,16 @@
 (ns app.operations
-  (:require [clj-time.core :as t]
-            [datomic.api :as d]
+  (:require [datomic.api :as d]
             [fulcro.server :as server :refer [defquery-root defquery-entity defmutation]]
             [fulcro.client.impl.application :as app]))
 
-;; TODO - deploy the in memory setup to Heroku
 ;; TODO - setup the database as part of a system start up
+;; TODO - setup proper logging
 ;; TODO - get meaningful calendars in place
 ;; TODO - deploy the postgres backed transactor setup to Heroku
 ;; TODO - figure out this set to vector business
 ;; TODO - get authentication in place
 
-(def db-uri "datomic:mem://core")
-
-(defn create-dummy-db
-  []
-  (let [schema [{:db/doc "A user's first name"
-                 :db/ident :user/first-name
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A user's last name"
-                 :db/ident :user/last-name
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A user's avatar url"
-                 :db/ident :user/avatar-url
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A user's calendars"
-                 :db/ident :user/calendars
-                 :db/valueType :db.type/ref
-                 :db/cardinality :db.cardinality/many
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A calendar's title"
-                 :db/ident :calendar/title
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A calendar's subtitle"
-                 :db/ident :calendar/subtitle
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A calendar's colour option"
-                 :db/ident :calendar/colour-option
-                 :db/valueType :db.type/keyword
-                 :db/cardinality :db.cardinality/one
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}
-                {:db/doc "A calendar's checked dates"
-                 :db/ident :calendar/checked-dates
-                 :db/valueType :db.type/string
-                 :db/cardinality :db.cardinality/many
-                 :db/id #db/id [:db.part/db]
-                 :db.install/_attribute :db.part/db}]
-        temp-ids (take 3 (repeatedly #(d/tempid :db.part/user)))
-        entities [{:db/id (nth temp-ids 0)
-                   :calendar/title "Some Title"
-                   :calendar/subtitle "some subtitle"
-                   :calendar/colour-option :a
-                   :calendar/checked-dates #{"20180110" "20180114" "20180122"}}
-                  {:db/id (nth temp-ids 1)
-                   :calendar/title "Another Title"
-                   :calendar/subtitle "some other subtitle"
-                   :calendar/colour-option :b
-                   :calendar/checked-dates #{"20180101" "20180114" "20180122"}}
-                  {:db/id (nth temp-ids 2)
-                   :user/first-name "Keigo"
-                   :user/last-name "Suzukawa"
-                   :user/avatar-url "images/avatar.jpg"
-                   :user/calendars #{(nth temp-ids 0) (nth temp-ids 1)}}]]
-
-    (d/delete-database db-uri)
-    (d/create-database db-uri)
-    @(d/transact (d/connect db-uri) schema)
-    @(d/transact (d/connect db-uri) entities)))
-
-(comment
+#_(comment
 
   (create-dummy-db)
 
@@ -113,33 +38,31 @@
 
   )
 
-;; TODO - make this part of the system startup
-(create-dummy-db)
-
-(defn get-current-user-id []
-  (->> (d/db (d/connect db-uri))
+(defn get-current-user-id [current-db]
+  (->> current-db
        (d/q '[:find ?e
               :where [?e
                       :user/first-name "Keigo"]])
        (ffirst)))
 
 (defquery-root :current-user
-  (value [{:keys [query]} params]
-         (let [db (d/db (d/connect db-uri))
-               current-user-id (get-current-user-id)]
-           (d/pull db query current-user-id))))
+  (value [{:keys [config db query]} params]
+         (let [{:keys [conn]} db
+               current-db (d/db conn)
+               current-user-id (get-current-user-id current-db)]
+           (d/pull current-db query current-user-id))))
 
 (defmutation add-checked-date!
   [{:keys [id date]}]
-  (action [_]
-          @(d/transact (d/connect db-uri)
+  (action [{:keys [config db]}]
+          @(d/transact (:conn db)
                        [[:db/add id :calendar/checked-dates date]])
           {}))
 
 (defmutation remove-checked-date!
   [{:keys [id date]}]
-  (action [_]
-          @(d/transact (d/connect db-uri)
+  (action [{:keys [config db]}]
+          @(d/transact (:conn db)
                       [[:db/retract id :calendar/checked-dates date]])
           {}))
 
--- src/main/app/server.clj
+++ src/main/app/server.clj
@@ -1,8 +1,97 @@
 (ns app.server
-  (:require [fulcro.easy-server :as easy-server]
-            [fulcro.server :as server]))
+  (:require [datomic.api :as d]
+            [fulcro.easy-server :as easy-server]
+            [fulcro.server :as server]
+            [com.stuartsierra.component :as component]))
+
+(def db-uri "datomic:mem://core")
+
+(def schema
+  [{:db/doc "A user's first name"
+    :db/ident :user/first-name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A user's last name"
+    :db/ident :user/last-name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A user's avatar url"
+    :db/ident :user/avatar-url
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A user's calendars"
+    :db/ident :user/calendars
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A calendar's title"
+    :db/ident :calendar/title
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A calendar's subtitle"
+    :db/ident :calendar/subtitle
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A calendar's colour option"
+    :db/ident :calendar/colour-option
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}
+   {:db/doc "A calendar's checked dates"
+    :db/ident :calendar/checked-dates
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/many
+    :db/id #db/id [:db.part/db]
+    :db.install/_attribute :db.part/db}])
+
+(def entities
+  [{:db/id (d/tempid :db.part/user -1)
+    :calendar/title "Some Title"
+    :calendar/subtitle "some subtitle"
+    :calendar/colour-option :a
+    :calendar/checked-dates #{"20180110" "20180114" "20180122"}}
+   {:db/id (d/tempid :db.part/user -2)
+    :calendar/title "Another Title"
+    :calendar/subtitle "some other subtitle"
+    :calendar/colour-option :b
+    :calendar/checked-dates #{"20180101" "20180114" "20180122"}}
+   {:db/id (d/tempid :db.part/user -3)
+    :user/first-name "Keigo"
+    :user/last-name "Suzukawa"
+    :user/avatar-url "images/avatar.jpg"
+    :user/calendars #{(d/tempid :db.part/user -1) (d/tempid :db.part/user -2)}}])
+
+
+(defrecord Db [config]
+  component/Lifecycle
+  (start [component]
+    (println "Starting database")
+    (let [db-uri (get-in config [:value :db-uri])
+          result (d/create-database db-uri)
+          conn (d/connect db-uri)]
+      @(d/transact conn schema)
+      @(d/transact conn entities)
+      (assoc component :conn conn)))
+
+  (stop [component]
+    (println ";; Stopping database")
+    (d/delete-database (get-in config [:value :db-uri]))
+    (assoc component :conn nil)))
 
 (defn make-system [config-path]
   (easy-server/make-fulcro-server
    :config-path config-path
-   :parser (server/fulcro-parser)))
+   :parser-injections #{:config :db}
+   :components {:db (component/using (map->Db {}) [:config])}))
