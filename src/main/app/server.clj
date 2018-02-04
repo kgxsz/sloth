5bc6c0e7050b4fe91ff4f63a9a4bd67652b7d0e4
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-02-04 14:34:18 +0000

    Refer to the db-uri

--- src/main/app/server.clj
+++ src/main/app/server.clj
@@ -10,7 +10,7 @@
   component/Lifecycle
   (start [component]
     (log/info "starting database.")
-    (let [db-uri (get-in config [:value :datomic-dev-uri])
+    (let [db-uri (get-in config [:value :db-uri])
           conn (do (d/create-database db-uri)
                    (d/connect db-uri))
           migrations [:sloth/user-schema
