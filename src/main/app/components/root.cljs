ba91052856ea80128cf40635af436f8d6cfc0930
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-02-15 19:22:53 +0000

    Naive navigation and routing

--- src/main/app/components/root.cljs
+++ src/main/app/components/root.cljs
@@ -4,11 +4,46 @@
             [app.components.user :refer [ui-user User]]
             [app.utils :as u]
             [fulcro.client.dom :as dom]
-            [fulcro.client.primitives :as prim :refer [defsc get-query]]))
+            [fulcro.client.primitives :as prim :refer [defsc get-query factory]]
+            [fulcro.client.routing :as r :refer-macros [defrouter]]))
 
-(defsc Root [this {:keys [ui/react-key ui/loading-data current-user]}]
-  {:query [:ui/react-key
+
+(defsc HomePage [this {:keys []}]
+  {:initial-state {:page :home-page}
+   :query [:page]}
+  (dom/div
+   #js {:onClick #(prim/transact! this `[(r/route-to {:handler :keigo-page})])}
+   (ui-logo)))
+
+
+(defsc KeigoPage [this {:keys [current-user]}]
+  {:initial-state {:page :keigo-page}
+   :query [:page
+           {[:current-user '_] (get-query User)}]}
+  (ui-user current-user))
+
+
+(defrouter Router :router
+  (ident [this props] [(:page props) :main])
+  :home-page HomePage
+  :keigo-page KeigoPage)
+
+
+(def ui-router (factory Router))
+
+
+(def routing-tree
+  (r/routing-tree
+    (r/make-route :home-page [(r/router-instruction :router [:home-page :main])])
+    (r/make-route :keigo-page [(r/router-instruction :router [:keigo-page :main])])))
+
+
+(defsc Root [this {:keys [ui/react-key ui/loading-data router current-user]}]
+  {:initial-state (fn [params]
+                    (merge routing-tree {:router (prim/get-initial-state Router {})}))
+   :query [:ui/react-key
            :ui/loading-data
+           {:router (prim/get-query Router)}
            {:current-user (get-query User)}]}
   (dom/div
    #js {:key react-key
@@ -16,5 +51,5 @@
    (dom/div
     #js {:className (u/bem [:page])}
     (if (or loading-data (empty? current-user))
-        (ui-logo)
-        (ui-user current-user)))))
+      (ui-logo)
+      (ui-router router)))))
