1caf7205757d08f1a29417e477770c2318d95360
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-02-17 17:39:06 +0000

    Get routing and pages into root

--- src/main/app/components/root.cljs
+++ src/main/app/components/root.cljs
@@ -2,22 +2,34 @@
   (:require [app.operations :as ops]
             [app.components.logo :refer [ui-logo]]
             [app.components.user :refer [ui-user User]]
+            [app.navigation :as navigation]
             [app.utils :as u]
             [fulcro.client.dom :as dom]
-            [fulcro.client.primitives :as prim :refer [defsc get-query factory]]
-            [fulcro.client.routing :as r :refer-macros [defrouter]]))
+            [fulcro.client.primitives :refer [defsc get-query get-initial-state factory]]
+            [fulcro.client.routing :refer-macros [defrouter]]))
+
+
+;; TODO
+;; Load user related data depending on the route param
+;; Get sane defaults into production, include them in the base migration
+;; Stop getting current user for now, it is all public
+;; Ensure that you can hit a frontend route on a page reload
+;; Intorduce an unknown-page handler and route
+;; Look at loading states and good feedback
 
 
 (defsc HomePage [this {:keys []}]
   {:initial-state {:page :home-page}
    :query [:page]}
   (dom/div
-   #js {:onClick #(prim/transact! this `[(r/route-to {:handler :keigo-page})])}
+   ;; TODO - wrap this in a mutation that loads what it needs and then does a navigation
+   #js {:onClick #(navigation/navigate {:handler :user-page
+                                        :route-params {:first-name "keigo"}})}
    (ui-logo)))
 
 
-(defsc KeigoPage [this {:keys [current-user]}]
-  {:initial-state {:page :keigo-page}
+(defsc UserPage [this {:keys [current-user]}]
+  {:initial-state {:page :user-page}
    :query [:page
            {[:current-user '_] (get-query User)}]}
   (ui-user current-user))
@@ -26,25 +38,18 @@
 (defrouter Pages :pages
   (ident [this props] [(:page props) :page])
   :home-page HomePage
-  :keigo-page KeigoPage)
+  :user-page UserPage)
 
 
 (def ui-pages (factory Pages))
 
 
-(def routing-tree
-  (r/routing-tree
-    (r/make-route :home-page [(r/router-instruction :pages [:home-page :page])])
-    (r/make-route :keigo-page [(r/router-instruction :pages [:keigo-page :page])])))
-
-
-(defsc Root [this {:keys [ui/react-key ui/loading-data pages current-user]}]
-  {:initial-state (fn [_] (merge routing-tree {:pages (prim/get-initial-state Pages {})}))
+(defsc Root [this {:keys [ui/react-key ui/loading-data current-user pages]}]
+  {:initial-state (fn [_] (merge navigation/routing-tree {:pages (get-initial-state Pages {})}))
    :query [:ui/react-key
            :ui/loading-data
            :current-user
-           {:pages (get-query Pages)}
-           ]}
+           {:pages (get-query Pages)}]}
   (dom/div
    #js {:key react-key
         :className (u/bem [:app])}
